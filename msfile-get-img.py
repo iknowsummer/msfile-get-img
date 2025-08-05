61761ba22afa36b9fe673551286aeb602de458f9
iknowsummer <iknowsummer@gmail.com>
2025-08-05 17:59:28 +0900

    [config] formatting設定

--- msfile-get-img.py
+++ msfile-get-img.py
@@ -4,11 +4,12 @@ import zipfile
 import shutil
 
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-dir_output = os.path.join(desktop_path,"msfiles")
-dir_temp = os.path.join(dir_output,"temp")
+dir_output = os.path.join(desktop_path, "msfiles")
+dir_temp = os.path.join(dir_output, "temp")
+
 
 def main():
-    #対象のファイルパスを取得
+    # 対象のファイルパスを取得
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
     else:
@@ -24,39 +25,42 @@ def main():
     # tempディレクトリ削除
     shutil.rmtree(dir_temp)
 
-    #完了後にフォルダを開く
+    # 完了後にフォルダを開く
     os.startfile(dir_output)
 
 
 def makeWorkfile(file_path):
     # デスクトップにディレクトリ"dir_output"を再作成
-    if os.path.exists(dir_output): shutil.rmtree(dir_output)
+    if os.path.exists(dir_output):
+        shutil.rmtree(dir_output)
     os.makedirs(dir_output, exist_ok=True)
     os.makedirs(dir_temp, exist_ok=True)
 
-    #zipファイルとして複製して解凍
-    zip_filepath = os.path.join(dir_temp,"ms.zip")
+    # zipファイルとして複製して解凍
+    zip_filepath = os.path.join(dir_temp, "ms.zip")
     shutil.copy(file_path, zip_filepath)
-    with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_filepath, "r") as zip_ref:
         zip_ref.extractall(dir_temp)
 
+
 def getMadiafiles():
-    #mediaディレクトリを取得
+    # mediaディレクトリを取得
     dir_media = None
     for root, dirs, files in os.walk(dir_temp):
-        if 'media' in dirs:
-            dir_media =  os.path.join(root, 'media')
+        if "media" in dirs:
+            dir_media = os.path.join(root, "media")
             break
 
     if dir_media is not None:
-        #mediaディレクトリのファイルをoutputへコピー
+        # mediaディレクトリのファイルをoutputへコピー
         for file_name in os.listdir(dir_media):
             source = os.path.join(dir_media, file_name)
             destination = os.path.join(dir_output, file_name)
-            shutil.copy2(source, destination) 
+            shutil.copy2(source, destination)
     else:
-        with open(os.path.join(dir_output,'noMedia.txt'), 'w') as f:
+        with open(os.path.join(dir_output, "noMedia.txt"), "w") as f:
             f.write('The directory "media" does not exist.\n')
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
